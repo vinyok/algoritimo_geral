67d0c27a0b6dfe999ce46d7c7a7caac76eb893c0
vinyok <vmarconsin@gmail.com>
2025-05-13 21:29:22 -0400

    teste

---
+++ Listas Git/ExFácil/1.py
@@ -0,0 +1,9 @@
+'''
+
+1. Crie uma lista com 5 números inteiros. Imprima o tamanho da lista usando len.
+
+'''
+
+num = [1, 2, 3, 4, 5]
+
+print (len (num))
---
+++ Listas Git/ExFácil/2.py
@@ -0,0 +1,12 @@
+'''
+2. Solicite ao usúario 5 nomes e armazene em uma lista. Depois, imprima cada nome
+em uma linha
+
+'''
+
+for i in range (5):
+    names_user = input('Digite um nome: ')
+
+nomes = names_user
+names_user
+
